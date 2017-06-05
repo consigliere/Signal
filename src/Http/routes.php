270ae0442dd75c4a5a4c0336dd6a5b5cb5be3300
anonymoussc <50c5ac69@opayq.com>
2017-06-05 07:36:54 +0700

    Modified src/Http/routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'signal', 'namespace' => 'App\\Components\Signal\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'signal', 'namespace' => 'App\\Components\Signal\Http\Controllers'], function () {
     Route::get('/', 'SignalController@index');
 });
