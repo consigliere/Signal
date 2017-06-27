14e1f4181ac1c5b598afbddcac978641d58155d0
anonymoussc <50c5ac69@opayq.com>
2017-06-27 19:03:14 +0700

    Deleted routes.php

--- src/Http/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'signal', 'namespace' => 'App\\Components\Signal\Http\Controllers'], function () {
-    Route::get('/', 'SignalController@index');
-});
