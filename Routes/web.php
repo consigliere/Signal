d416302fb56129281e8754b0a76a8257b5d55c7d
anonymoussc <50c5ac69@opayq.com>
2018-02-05 01:02:59 +0700

    Modified web Routes

--- Routes/web.php
+++ Routes/web.php
@@ -5,5 +5,18 @@
  */
 
 Route::group(['middleware' => 'web', 'prefix' => 'signal', 'namespace' => 'App\\Components\Signal\Http\Controllers'], function () {
-    // Route::get('/', 'SignalController@index');
+    Route::get('/', function() {
+        \Event::fire('event.info', [['message' => 'test3']]);
+        echo 'success';
+    });
+});
+
+Route::get('/signal/mail', function () {
+
+    \Mail::raw('hello world', function($message) {
+        $message->subject('message subject')->to('test@example.org');
+    });
+
+    //return new App\Components\Signal\Emails\SignalMailer(['test' =>'test']);
+    //Illuminate\Support\Facades\Mail::to(config('50c5ac69@opayq.com'))->send(new App\Components\Signal\Emails\SignalMailer(['test' =>'test']));
 });
