16a63f55a8f3e38a7405d07fc782ec77c04d7cde
anonymoussc <50c5ac69@opayq.com>
2018-02-11 03:12:18 +0700

    Modified config Config

--- Config/config.php
+++ Config/config.php
@@ -1,25 +1,26 @@
 <?php
 
 return [
-    'name'        => 'Signal',
+    'name' => 'Signal',
 
-    // Enable logging activity
-    'logActivity' => env('LOG_ACTIVITY', true),
-
-    // Specific logging control when logActivity option enabled
-    'emergency'   => false,
-    'alert'       => false,
-    'critical'    => false,
-    'error'       => true,
-    'warning'     => false,
-    'notice'      => false,
-    'info'        => false,
-    'debug'       => false,
+    'log'   => [
+        // Set to 'true' to enable logging activity
+        'activity'  => env('LOG_ACTIVITY', true),
+        // Specific logging control when log.activity option enabled
+        'emergency' => false,
+        'alert'     => false,
+        'critical'  => false,
+        'error'     => true,
+        'warning'   => false,
+        'notice'    => false,
+        'info'      => false,
+        'debug'     => false,
+    ],
 
     // Sent log data to email
     'email' => [
         // Set to 'true' to sent log data to email
         'sent'   => env('SIGNAL_EMAIL_SENT', false),
-        'sentTo' => env('SIGNAL_EMAIL_SENT_TO', 'example@example.io'),
+        'sentTo' => env('SIGNAL_EMAIL_SENT_TO', 'noreply@example.com'),
     ],
 ];
